b25669574a9d08ead844e16dd64f06f5428cfc86
Artem1994-Artem <56757925+Artem1994-Artem@users.noreply.github.com>
2021-08-23 14:09:20 +0500

    Add files via upload

---
+++ C++Lab7/C++Lab7_2/C++Lab7_2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+
+class FirstClass    // базовый класс
+{
+protected:          // спецификатор доступа к элементу value
+    int value;
+public:
+    FirstClass()
+    {
+        value = 0;
+    }
+
+    FirstClass(int input)
+    {
+        value = input;
+    }
+
+    void show_value()
+    {
+        cout << value << endl;
+    }
+};
+
+class SecondClass : public FirstClass   // производный класс
+{
+public:
+    SecondClass() : FirstClass()   // конструктор класса SecondClass вызывает конструктор класса FirstClass
+    {}
+
+    SecondClass(int inputS) : FirstClass(inputS)   // inputS передается в конструктор с параметром класса FirstClass
+    {}
+
+    virtual void ValueSqr() // возводит value в квадрат. 
+    {
+        value *= value;
+    }
+};
+class ThirdClass : public SecondClass {
+public:
+    ThirdClass() : SecondClass() {}
+    ThirdClass(int inputS) : SecondClass(inputS) {}
+    void ValueSqr() override {
+        value = value * value * value;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    FirstClass F_object(3);     // объект базового класса
+    cout << "value F_object = ";
+    F_object.show_value();
+
+    SecondClass S_object(4);    // объект производного класса
+    cout << "value S_object = ";
+    S_object.show_value();  // вызов метода базового класса
+
+    S_object.ValueSqr();        // возводим value в квадрат
+    cout << "квадрат value S_object = ";
+    S_object.show_value();
+
+    ThirdClass T_object(2);
+    cout << "value T_object = ";
+    T_object.show_value();
+
+    T_object.ValueSqr();        // возводим value в куб
+    cout << "куб value T_object = ";
+    T_object.show_value();
+    
+    cout << endl;
+}
---
+++ C++Lab7/C++Lab7_3/C++Lab7_3.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+class FirstClass    // базовый класс
+{
+public:
+    virtual void show_value() = 0;
+    virtual void ValueSqr() = 0;
+};
+
+class SecondClass : public FirstClass   // производный класс
+{
+protected:          
+    int value;
+public:
+    SecondClass()   // конструктор класса SecondClass вызывает конструктор класса FirstClass
+    {
+        value = 0;
+    }
+
+    SecondClass(int inputS)   // inputS передается в конструктор с параметром класса FirstClass
+    {
+        value = inputS;
+    }
+    void show_value() override {
+        cout << "value S_object = " << value << endl;
+     
+    }
+
+     void ValueSqr() override// возводит value в квадрат. 
+    {
+        value *= value;
+    }
+};
+class ThirdClass : public FirstClass {
+protected:          // спецификатор доступа к элементу value
+    double value;
+public:
+    ThirdClass() { value = 0; }
+    ThirdClass(double inputS) { value = inputS; }
+    void ValueSqr() override {
+        value = value * value * value;
+    }
+    void show_value() override {
+        cout << "value T_object = " << value << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    SecondClass S_object(4);    // объект производного класса
+    S_object.show_value();  // вызов метода базового класса
+    S_object.ValueSqr();        // возводим value в квадрат
+    cout << "Квадрат ";
+    S_object.show_value();
+
+    ThirdClass T_object(2.5);
+    T_object.show_value();
+    T_object.ValueSqr();        // возводим value в куб
+    cout << "Куб ";
+    T_object.show_value();
+
+    cout << endl;
+}
